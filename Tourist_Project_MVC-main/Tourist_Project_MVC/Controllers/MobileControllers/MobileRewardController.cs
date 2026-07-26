25cb7066ae0e7fdcca156e3c8db4df4ee45c8881
Ewis2011 <aewis2011@gmail.com>
2026-07-26 04:38:24 +0300

    mobile controllers

---
+++ Tourist_Project_MVC-main/Tourist_Project_MVC/Controllers/MobileControllers/MobileRewardController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tourist_Project_MVC.Data;
+using Tourist_Project_MVC.Repositories;
+
+namespace Tourist_Project_MVC.Controllers.MobileControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MobileRewardController : ControllerBase
+    {
+        private readonly TouristContext _context;
+        private readonly IRewardRepository reward;
+        public MobileRewardController(TouristContext context, IRewardRepository reward)
+        {
+            _context = context;
+            this.reward = reward;
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult AllRewards()
+        {
+            var missions = reward.GetAll();
+            if (missions != null)
+            {
+                return Ok(missions);
+            }
+            return BadRequest(new { message = "Unable to retrieve rewards." });
+        }
+    }
+}
