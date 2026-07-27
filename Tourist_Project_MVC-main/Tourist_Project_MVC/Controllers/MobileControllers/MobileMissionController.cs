df064d590dd7131f119ef3bcd4f689731363e0a7
Ewis2011 <aewis2011@gmail.com>
2026-07-27 06:34:00 +0300

    fetch and save

--- Tourist_Project_MVC-main/Tourist_Project_MVC/Controllers/MobileControllers/MobileMissionController.cs
+++ Tourist_Project_MVC-main/Tourist_Project_MVC/Controllers/MobileControllers/MobileMissionController.cs
@@ -1,11 +1,95 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tourist_Project_MVC.Data;
+using Tourist_Project_MVC.DTOs;
+using Tourist_Project_MVC.Models;
+using Tourist_Project_MVC.Repositories;
 
 namespace Tourist_Project_MVC.Controllers.MobileControllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+
     public class MobileMissionController : ControllerBase
     {
+        private readonly TouristContext _context;
+        private readonly IMissionRepository _missionRepo;
+        private readonly ITouristRepository _touristRepo;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public MobileMissionController(
+            TouristContext context,
+            IMissionRepository missionRepository,
+            ITouristRepository touristRepository,
+            UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _missionRepo = missionRepository;
+            _touristRepo = touristRepository;
+            _userManager = userManager;
+        }
+        [HttpGet("AllMissions")]
+        public IActionResult AllMissions()
+        {
+            var missions = _missionRepo.GetAll();
+
+            if (missions == null)
+            {
+                return BadRequest(new { message = "Unable to retrieve missions." });
+            }
+
+            var missionDto = missions.Select(m => new MissionDTO
+                {
+                    Id = m.Id,
+                    Title= m.Title,
+                    Desc= m.Description,
+                    Points= m.PointsReward,
+                    MissDestId = m.DestinationId,
+                    Type= m.MissionType
+                }).ToList();
+                return Ok(missionDto);
+        }
+        [HttpPost("Complete")]
+        public async Task<IActionResult> CompleteMission([FromBody] CompleteMissionDto dto)
+        {
+            var allClaims = User.Claims.Select(c => new { c.Type, c.Value }).ToList();
+            var applicationUser = await _userManager.GetUserAsync(User);
+            if(applicationUser == null)
+            {
+                return Unauthorized();
+            }
+            var tourist = _touristRepo.GetOrCreateByApplicationUser(applicationUser);
+
+            var mission = await _context.Missions.FindAsync(dto.MissionId);
+            if(mission == null)
+            {
+                return NotFound(new { message = "Mission not found" });
+            }
+            var alreadyCompleted = await _context.UserMissions.AnyAsync(um => um.TouristId == tourist.Id && um.MissionId == dto.MissionId);
+            if (alreadyCompleted)
+            {
+                return Conflict(new { message = "Mission already completed." });
+            }
+            var userMission = new UserMission()
+            {
+                TouristId = tourist.Id,
+                MissionId = dto.MissionId,
+                Status = "Completed",
+                PointsEarned = mission.PointsReward,
+                Completed_At = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Unspecified)
+            };
+            _context.UserMissions.Add(userMission);
+            await _context.SaveChangesAsync();
+            return Ok(new
+            {
+                message="Mission Completed successfully",
+                pointsEarned = userMission.PointsEarned
+            });
+        }
     }
 }
---
+++ Tourist_Project_MVC-main/Tourist_Project_MVC/DTOs/RewardDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Tourist_Project_MVC.DTOs
+{
+    public class RewardDTO
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public int Points { get; set; }
+        public int Quntity { get; set; }
+        public DateTime Expiration { get; set; }
+        public string Status { get; set; }
+    }
+}
